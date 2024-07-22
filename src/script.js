0bdbb62012422a9b6d22960dd9a6e0482a188aef
Sergio <44326862+sergioqa123@users.noreply.github.com>
2024-07-21 21:12:18 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,117 @@
+let tuScoreTexto = document.querySelector("#tuScore");
+let pcScoreTexto = document.querySelector("#pcScore");
+let resultadoTexto = document.querySelector("#resultado");
+let boton1 = document.querySelector("#boton1");
+let boton2 = document.querySelector("#boton2");
+let boton3 = document.querySelector("#boton3");
+
+// Valores iniciales
+let tuScore = 0;
+let pcScore = 0;
+let resultado = "Presiona un botón para empezar."
+let modoActual = 0;
+
+const objetos = [
+    {
+        nombre: "piedra",
+        ganaContra: "tijeras",
+        pierdeContra: "papel"
+    },
+    {
+        nombre: "papel",
+        ganaContra: "piedra",
+        pierdeContra: "tijeras"
+    },
+    {
+        nombre: "tijeras",
+        ganaContra: "papel",
+        pierdeContra: "piedra"
+    }
+]
+
+const modos = [
+    {
+        nombre: "inicio",
+        "texto de botones": ["PIEDRA", "PAPEL", "TIJERAS"],
+        "funciones de botones": [jugarPiedra, jugarPapel, jugarTijeras]
+    },
+    {
+        nombre: "perdiste",
+        "texto de botones": ["CONTINUAR", "REINICIAR", "REINICIAR"],
+        "funciones de botones": [continuar, reiniciar, reiniciar]
+    }
+]
+
+boton1.onclick = jugarPiedra;
+boton2.onclick = jugarPapel;
+boton3.onclick = jugarTijeras;
+
+function pcElige(){
+    return objetos[Math.floor(Math.random() * 3)];
+}
+
+function jugar(objeto) {
+    let eleccionPc = pcElige();
+    let eleccionTu = objetos[objeto];
+    if(eleccionTu.nombre == eleccionPc.pierdeContra) {
+        resultado = "Ganaste\n";
+        tuScore++;
+    } else if (eleccionTu.nombre === eleccionPc.ganaContra) {
+        resultado = "Perdiste\n";
+        pcScore++;
+        modoActual++;
+        decidir();
+    } else {
+        resultado = "Empate\n";
+    }
+    tuScoreTexto.innerText = tuScore;
+    pcScoreTexto.innerText = pcScore;
+    resultadoTexto.innerText = resultado + "\nTú escogiste: " + eleccionTu.nombre + ".\nPC escogió: " + eleccionPc.nombre;
+}
+
+//Funciones de elección
+function jugarPiedra(){
+    jugar(0);
+}
+
+function jugarPapel(){
+    jugar(1);
+}
+
+function jugarTijeras(){
+    jugar(2);
+}
+
+//Funciones de decisión
+function continuar(){
+    modoActual--;
+    cargarValores();
+}
+
+function reiniciar(){
+    tuScore = 0;
+    pcScore = 0;
+    tuScoreTexto.innerText = tuScore;
+    pcScoreTexto.innerText = pcScore;
+    continuar();
+}
+
+function decidir(){
+    cargarValores();
+}
+
+function cargarValores() {
+    if(modoActual){
+        boton3.style.display = "none";
+    } else {
+        boton3.style.display = "block";
+    }
+    boton1.innerText = modos[modoActual]["texto de botones"][0];
+    boton2.innerText = modos[modoActual]["texto de botones"][1];
+    boton3.innerText = modos[modoActual]["texto de botones"][2];
+    boton1.onclick = modos[modoActual]["funciones de botones"][0];
+    boton2.onclick = modos[modoActual]["funciones de botones"][1];
+    boton3.onclick = modos[modoActual]["funciones de botones"][2];
+}
+
+cargarValores();
